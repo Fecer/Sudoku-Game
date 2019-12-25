7ba2972814d913c0e442fa5575c039282829d882
Fever <victorxzn@qq.com>
2019-12-25 14:10:35 +0800

    输入情况判断
    判断用户输入的合法性，走向不同分支

---
+++ SudokuGenerator/SudokuGenerator/Sudoku.h
@@ -0,0 +1,18 @@
+//
+//  Sudoku.h
+//  SudokuGenerator
+//
+//  Created by Fever on 2019/12/25.
+//  Copyright © 2019 BIT. All rights reserved.
+//
+
+#ifndef Sudoku_h
+#define Sudoku_h
+
+
+class Sudoku
+{
+    
+};
+
+#endif /* Sudoku_h */
--- SudokuGenerator/SudokuGenerator/main.cpp
+++ SudokuGenerator/SudokuGenerator/main.cpp
@@ -7,9 +7,50 @@
 //
 
 #include <iostream>
+#include <string>
+#include "Sudoku.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define MAX 1000000
+#define MIN 1
+
+
+int main(int argc, const char * argv[])
+{
+    
+    //参数数量合法检测
+    if(argc != 3)
+    {
+        cout << "Wrong argument quantity!" << endl;
+        return 0;
+    }
+ 
+    string oprationType = argv[1];  //查看操作类型
+    
+    if(oprationType == "-c")    //生成终局
+    {
+        int num = atoi(argv[2]);
+        
+        if(num >= MIN && num <= MAX)    //在合理范围内，生成num个终局
+        {
+            
+        }
+        else
+        {
+            cout << "Illegal result quantity\n" << endl;
+        }
+    }
+    else if(oprationType == "-s")   //求解数独
+    {
+        string path = argv[2];
+        
+        
+    }
+    else    //参数正确性合法检测
+    {
+        cout << "Illegal input" << endl;
+        return 0;
+    }
+    
     return 0;
 }
