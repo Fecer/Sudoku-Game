16d87510bafa1aa6b1abb835ebff5ab049bdd550
Fever <victorxzn@qq.com>
2019-12-25 11:29:33 +0800

    项目更改

---
+++ SudokuGenerator/SudokuGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SudokuGenerator
+//
+//  Created by Fever on 2019/12/25.
+//  Copyright © 2019 BIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
