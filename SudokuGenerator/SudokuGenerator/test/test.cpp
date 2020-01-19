64dc9aa91771093960b12452e50412d38a0ac755
Fever <victorxzn@qq.com>
2020-01-19 14:46:49 +0800

    完成单元测试

--- SudokuGenerator/SudokuGenerator/test/test.cpp
+++ SudokuGenerator/SudokuGenerator/test/test.cpp
@@ -8,10 +8,11 @@
 
 #include "Sudoku.h"
 #include "endingGenerator.h"
+#include <fstream>
 
 void RtoSqrTEST()
 {
-    string str = "6 1 2 3 4 5 7 8 9\n3 4 5 7 8 9 6 1 2\n7 8 9 6 1 2 3 4 5\n1 2 3 4 5 7 8 9 6\n4 5 7 8 9 6 1 2 3\n8 9 6 1 2 3 4 5 7\n2 3 4 5 7 8 9 6 1\n5 7 8 9 6 1 2 3 4\n9 6 1 2 3 4 5 7 8";
+    string str = "6 1 2 3 4 5 7 8 9\n3 4 5 7 8 9 6 1 2\n7 8 9 6 1 2 3 4 5\n1 2 3 4 5 7 8 9 6\n4 5 7 8 9 6 1 2 3\n8 9 6 1 2 3 4 5 7\n2 3 4 5 7 8 9 6 1\n5 7 8 9 6 1 2 3 4\n9 6 1 2 3 4 5 7 8\n\n";
     
     eGenerator eg;
     Sudoku s(1);
@@ -28,14 +29,22 @@ void RtoSqrTEST()
 
 void solveTEST()
 {
-    Sudoku s("./solveTest.txt");
+    
+    fstream fs("./sudokuAnswer.txt" );     // 创建个文件流对象,并打开txt
+    stringstream ss;                        // 创建字符串流对象
+    ss << fs.rdbuf();                       // 把文件流中的字符输入到字符串流中
+    string str = ss.str();                  // 获取流中的字符串
+    
+    str += "\n\n";
+    Sudoku s("./testsolve.txt");
     
     s.solve();
     
+    if(str == s.getOutputE())
+        cout << "solve Right" << endl;
+    else
+        cout << "solve Wrong" << endl;
+        
+    return;
 }
 
-int main
-{
-    
-    return 0;
-}
